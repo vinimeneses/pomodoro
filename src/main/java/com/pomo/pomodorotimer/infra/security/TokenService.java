4536b3f560e017898ba4cff0948b14eeb98342a8
Vinícius Meneses <viniciusmenesesdev@gmail.com>
2024-05-11 09:19:28 -0300

    remove comments

--- src/main/java/com/pomo/pomodorotimer/infra/security/TokenService.java
+++ src/main/java/com/pomo/pomodorotimer/infra/security/TokenService.java
@@ -3,7 +3,6 @@ package com.pomo.pomodorotimer.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.pomo.pomodorotimer.dto.UserDto;
 import com.pomo.pomodorotimer.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
